09ac4c6c53faeaeaec875874a9b6db5e57b3957f
Pierre Rolland <roll.pierre@gmail.com>
2020-10-27 09:14:50 +0100

    Debug

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -22,6 +22,8 @@ import CategoryLink from '@/components/CategoryLink';
 export default {
   components: {CategoryLink, Logo},
   async asyncData({ app }) {
+    console.log(app.$axios.defaults);
+    return { categories: [] };
     let categories = (await app.$axios.get(`${app.$axios.defaults.baseURL}/categories`)).data;
 
     return { categories };
