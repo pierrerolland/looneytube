2644b9b8d0371c9da2aae97f02f114f42051373f
Pierre Rolland <roll.pierre@gmail.com>
2021-08-01 16:26:31 +0200

    Added video layout

---
+++ front/layouts/video.vue
@@ -0,0 +1,20 @@
+<template>
+  <Nuxt />
+</template>
+
+<style>
+html {
+  box-sizing: border-box;
+}
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+html, body, __nuxt, __layout {
+  margin: 0;
+  height: 100vh;
+  width: 100vw;
+}
+</style>
